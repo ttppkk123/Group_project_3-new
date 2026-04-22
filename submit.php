b8a9d35303175639a6c6e2e178e9410531ee2f10
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-04-23 00:20:43 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -11,11 +11,15 @@
         $name = $_POST['patient_name'];
         $phone = $_POST['phone'];
         $doctor = $_POST['doctor_name'];
+        $symptoms = $_POST['symptoms'];
+        $date = $_POST['appointment_date'];
 
         echo "<h1>თქვენი ჯავშანი მიღებულია!</h1>";
         echo "<p><b>პაციენტი:</b> " . $name . "</p>";
         echo "<p><b>ტელეფონი:</b> " . $phone . "</p>";
         echo "<p><b>ექიმი:</b> " . $doctor . "</p>";
+        echo "<p><b>სიმპტომები:</b> " . $symptoms . "</p>";
+        echo "<p><b>თარიღი:</b> " . $date . "</p>";
         
         echo "<br><hr>";
         echo "<h3>Confirmation: Your appointment is submitted.</h3>";
