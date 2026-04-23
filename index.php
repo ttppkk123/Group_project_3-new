a7db4cc9ce1182cc3b8726bc32e07ea1aa3bb303
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-04-23 06:23:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -348,92 +348,28 @@
       </div>
       <div class="carousel-wrap ">
         <div class="owl-carousel team_carousel">
+          <?php foreach ($doctors as $doc): ?>
           <div class="item">
             <div class="box">
               <div class="img-box">
-                <img src="images/team1.jpg" alt="" />
+                <img src="<?php echo $doc['img']; ?>" alt="" />
               </div>
               <div class="detail-box">
                 <h5>
-                  Hennry
+                  <?php echo $doc['name']; ?>
                 </h5>
                 <h6>
-                  MBBS
+                  <?php echo $doc['spec']; ?>
                 </h6>
                 <div class="social_box">
-                  <a href="">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
+                  <a href="<?php echo $doc['facebook']; ?>">
+                  <a href="<?php echo $doc['twitter']; ?>">
+                  <a href="<?php echo $doc['linkedin']; ?>">
+                  <a href="<?php echo $doc['instagram']; ?>">
                 </div>
               </div>
             </div>
-          </div>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/team2.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Jenni
-                </h5>
-                <h6>
-                  MBBS
-                </h6>
-                <div class="social_box">
-                  <a href="">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/team3.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Morco
-                </h5>
-                <h6>
-                  MBBS
-                </h6>
-                <div class="social_box">
-                  <a href="">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
+          <?php endforeach; ?>
           </div>
         </div>
       </div>
