c290ef039a24e9a1879bdfa3c6fd088fe1eb1a71
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-04-23 11:21:50 +0200

    Update and rename contact.html to contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
+<?php include 'data.php'; ?>
 <!DOCTYPE html>
 <html>
 
@@ -48,13 +49,13 @@
             <a href="">
               <i class="fa fa-phone" aria-hidden="true"></i>
               <span>
-                Call : +01 123455678990
+                Call : <?php echo $site['phone']; ?>
               </span>
             </a>
             <a href="">
               <i class="fa fa-envelope" aria-hidden="true"></i>
               <span>
-                Email : demo@gmail.com
+                Email : <?php echo $site['email']; ?>
               </span>
             </a>
             <a href="">
@@ -69,10 +70,10 @@
       <div class="header_bottom">
         <div class="container-fluid">
           <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <a class="navbar-brand" href="index.html">
-              <img src="images/logo.png" alt="">
+            <a class="navbar-brand" href="index.php">
+              <img src="<?php echo $site['logo']; ?>" alt="">
             </a>
-            </a>
+
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class=""> </span>
@@ -81,24 +82,18 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <div class="d-flex mr-auto flex-column flex-lg-row align-items-center">
                 <ul class="navbar-nav  ">
-                  <li class="nav-item ">
-                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="about.html"> About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="treatment.html">Treatment</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="doctor.html">Doctors</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="testimonial.html">Testimonial</a>
-                  </li>
-                  <li class="nav-item active">
-                    <a class="nav-link" href="contact.html">Contact Us</a>
-                  </li>
+                  <?php foreach ($nav as $item): ?>
+                    <li class="nav-item <?php echo !empty($item['active']) ? 'active' : ''; ?>">
+                    <a class="nav-link" href="<?php echo $item['url']; ?>">
+                      <?php echo $item['title']; ?>
+              
+                      <?php if (!empty($item['active'])): ?>
+                        <span class="sr-only">(current)</span>
+                      <?php endif; ?>
+      
+                      </a>
+                    </li>
+                  <?php endforeach; ?>
                 </ul>
               </div>
               <div class="quote_btn-container">
@@ -128,7 +123,6 @@
     <!-- end header section -->
   </div>
 
-
   <!-- contact section -->
   <section class="contact_section layout_padding-bottom">
     <div class="container">
@@ -140,24 +134,12 @@
       <div class="row">
         <div class="col-md-7">
           <div class="form_container">
-            <form action="">
-              <div>
-                <input type="text" placeholder="Full Name" />
-              </div>
-              <div>
-                <input type="email" placeholder="Email" />
-              </div>
-              <div>
-                <input type="text" placeholder="Phone Number" />
-              </div>
-              <div>
-                <input type="text" class="message-box" placeholder="Message" />
-              </div>
-              <div class="btn_box">
-                <button>
-                  SEND
-                </button>
-              </div>
+           <form action="contact_success.php" method="POST">
+              <input type="text" name="name" placeholder="Name" required>
+              <input type="email" name="email" placeholder="Email" required>
+              <input type="text" name="phone" placeholder="Phone">
+              <textarea name="message" placeholder="Message"></textarea>
+              <button type="submit">Send</button>
             </form>
           </div>
         </div>
@@ -171,13 +153,13 @@
   </section>
   <!-- end contact section -->
 
-  <!-- info section -->
+<!-- info section -->
   <section class="info_section ">
     <div class="container">
       <div class="info_top">
         <div class="info_logo">
           <a href="">
-            <img src="images/logo.png" alt="">
+            <img src="<?php echo $site['logo']; ?>" alt="">
           </a>
         </div>
         <div class="info_form">
@@ -198,113 +180,80 @@
             <div class="info_contact">
               <a href="">
                 <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span>
-                  Location
-                </span>
+                <span><?php echo $site['address']; ?></span>
               </a>
               <a href="">
                 <i class="fa fa-phone" aria-hidden="true"></i>
-                <span>
-                  Call +01 1234567890
-                </span>
+                <span><?php echo $site['phone']; ?></span>
               </a>
               <a href="">
                 <i class="fa fa-envelope"></i>
-                <span>
-                  demo@gmail.com
-                </span>
+                <span><?php echo $site['email']; ?></span>
               </a>
             </div>
+            
+            <!-- SOCIAL BOX -->
             <div class="social_box">
-              <a href="">
+              <a href="<?php echo $site['social']['facebook']; ?>">
                 <i class="fa fa-facebook" aria-hidden="true"></i>
               </a>
-              <a href="">
+              <a href="<?php echo $site['social']['twitter']; ?>">
                 <i class="fa fa-twitter" aria-hidden="true"></i>
               </a>
-              <a href="">
+              <a href="<?php echo $site['social']['linkedin']; ?>">
                 <i class="fa fa-linkedin" aria-hidden="true"></i>
               </a>
-              <a href="">
+              <a href="<?php echo $site['social']['instagram']; ?>">
                 <i class="fa fa-instagram" aria-hidden="true"></i>
               </a>
             </div>
           </div>
+          <!-- USEFUL LINKS -->
           <div class="col-md-6 col-lg-3">
             <div class="info_links">
               <h5>
                 Useful link
               </h5>
               <div class="info_links_menu">
-                <a href="index.html">
-                  Home
-                </a>
-                <a href="about.html">
-                  About
-                </a>
-                <a href="treatment.html">
-                  Treatment
-                </a>
-                <a href="doctor.html">
-                  Doctors
-                </a>
-                <a href="testimonial.html">
-                  Testimonial
-                </a>
-                <a class="active" href="contact.html">
-                  Contact us
-                </a>
+                <?php foreach ($links as $link): ?>
+                  <a href="<?php echo $link['url']; ?>">
+                    <?php echo $link['title']; ?>
+                  </a>
+                <?php endforeach; ?>
               </div>
             </div>
           </div>
+          <!-- LATEST POSTS -->
           <div class="col-md-6 col-lg-3">
             <div class="info_post">
               <h5>
                 LATEST POSTS
               </h5>
-              <div class="post_box">
-                <div class="img-box">
-                  <img src="images/post1.jpg" alt="">
+              <?php foreach ($latest_posts as $post): ?>
+                <div class="post_box">
+                  <div class="img-box">
+                    <img src="<?php echo $post['img']; ?>" alt="">
+                  </div>
+                  <p><?php echo $post['text']; ?></p>
                 </div>
-                <p>
-                  Normal
-                  distribution
-                </p>
-              </div>
-              <div class="post_box">
-                <div class="img-box">
-                  <img src="images/post2.jpg" alt="">
-                </div>
-                <p>
-                  Normal
-                  distribution
-                </p>
-              </div>
+              <?php endforeach; ?>
             </div>
           </div>
+
+          <!-- NEWS -->
           <div class="col-md-6 col-lg-3">
             <div class="info_post">
               <h5>
                 News
               </h5>
-              <div class="post_box">
-                <div class="img-box">
-                  <img src="images/post3.jpg" alt="">
+              <?php foreach ($news as $post): ?>
+                <div class="post_box">
+                  <div class="img-box">
+                    <img src="<?php echo $post['img']; ?>"" alt="">
+                  </div>
+                  <p><?php echo $post['text']; ?></p>
                 </div>
-                <p>
-                  Normal
-                  distribution
-                </p>
-              </div>
-              <div class="post_box">
-                <div class="img-box">
-                  <img src="images/post4.png" alt="">
-                </div>
-                <p>
-                  Normal
-                  distribution
-                </p>
-              </div>
+              <?php endforeach; ?>
             </div>
           </div>
         </div>
@@ -312,6 +261,7 @@
     </div>
   </section>
   <!-- end info_section -->
+
 
 
   <!-- footer section -->
