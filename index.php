8e39e061965417f65fb506c7e88135851ace6f03
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-04-23 06:44:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -349,23 +349,25 @@
       <div class="carousel-wrap ">
         <div class="owl-carousel team_carousel">
           <?php foreach ($doctors as $doc): ?>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="<?php echo $doc['img']; ?>" alt="" />
-              </div>
-              <div class="detail-box">
-                <h5>
-                  <?php echo $doc['name']; ?>
-                </h5>
-                <h6>
-                  <?php echo $doc['spec']; ?>
-                </h6>
-                <div class="social_box">
-                  <a href="<?php echo $doc['facebook']; ?>"><i class="fa fa-facebook"></i></a>
-                  <a href="<?php echo $doc['twitter']; ?>"><i class="fa fa-twitter"></i></a>
-                  <a href="<?php echo $doc['linkedin']; ?>"><i class="fa fa-linkedin"></i></a>
-                  <a href="<?php echo $doc['instagram']; ?>"><i class="fa fa-instagram"></i></a>
+            <div class="carousel-item <?php if ($index === 0) echo 'active'; ?>">
+            <div class="item">
+              <div class="box">
+                <div class="img-box">
+                  <img src="<?php echo $doc['img']; ?>" alt="" />
+                </div>
+                <div class="detail-box">
+                  <h5>
+                    <?php echo $doc['name']; ?>
+                  </h5>
+                  <h6>
+                    <?php echo $doc['spec']; ?>
+                  </h6>
+                  <div class="social_box">
+                    <a href="<?php echo $doc['facebook']; ?>"><i class="fa fa-facebook"></i></a>
+                    <a href="<?php echo $doc['twitter']; ?>"><i class="fa fa-twitter"></i></a>
+                    <a href="<?php echo $doc['linkedin']; ?>"><i class="fa fa-linkedin"></i></a>
+                    <a href="<?php echo $doc['instagram']; ?>"><i class="fa fa-instagram"></i></a>
+                  </div>
                 </div>
               </div>
             </div>
