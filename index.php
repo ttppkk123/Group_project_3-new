47788cad0ae139df7f4550aeb7e27bc24b56f480
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-04-23 07:40:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -49,13 +49,13 @@
             <a href="">
               <i class="fa fa-phone" aria-hidden="true"></i>
               <span>
-                Call : +01 123455678990
+                Call : <?php echo $site['phone']; ?>
               </span>
             </a>
             <a href="">
               <i class="fa fa-envelope" aria-hidden="true"></i>
               <span>
-                Email : demo@gmail.com
+                Email : <?php echo $site['email']; ?>
               </span>
             </a>
             <a href="">
@@ -82,24 +82,18 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <div class="d-flex mr-auto flex-column flex-lg-row align-items-center">
                 <ul class="navbar-nav  ">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+
+                  <li class="nav-item <?php echo !empty($item['active']) ? 'active' : ''; ?>">
+                  <a class="nav-link" href="<?php echo $item['url']; ?>">
+                    <?php echo $item['title']; ?>
+            
+                    <?php if (!empty($item['active'])): ?>
+                      <span class="sr-only">(current)</span>
+                    <?php endif; ?>
+    
+                    </a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="about.html"> About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="treatment.html">Treatment</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="doctor.html">Doctors</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="testimonial.html">Testimonial</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact Us</a>
-                  </li>
+                <?php endforeach; ?>
                 </ul>
               </div>
               <div class="quote_btn-container">
@@ -555,7 +549,7 @@
                   <div class="img-box">
                     <img src="<?php echo $post['img']; ?>"" alt="">
                   </div>
-                  <p><?php echo $post['text']; ?>/p>
+                  <p><?php echo $post['text']; ?></p>
                 </div>
               <?php endforeach; ?>
             </div>
