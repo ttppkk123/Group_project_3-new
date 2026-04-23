67bd7f7d584553edb4ed2b4df1802f0d5728c667
ttppkk123 <62799607+ttppkk123@users.noreply.github.com>
2026-04-23 04:08:27 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -6,28 +6,31 @@
 </head>
 <body>
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_POST['patient_name'];
-        $phone = $_POST['phone'];
-        $doctor = $_POST['doctor_name'];
-        $symptoms = $_POST['symptoms'];
-        $date = $_POST['appointment_date'];
-
-        echo "<h1>თქვენი ჯავშანი მიღებულია!</h1>";
-        echo "<p><b>პაციენტი:</b> " . $name . "</p>";
-        echo "<p><b>ტელეფონი:</b> " . $phone . "</p>";
-        echo "<p><b>ექიმი:</b> " . $doctor . "</p>";
-        echo "<p><b>სიმპტომები:</b> " . $symptoms . "</p>";
-        echo "<p><b>თარიღი:</b> " . $date . "</p>";
+    <div class="confirmation_container">
         
-        echo "<br><hr>";
-        echo "<h3>Confirmation: Your appointment is submitted.</h3>";
-    } else {
-        echo "ფორმა არ არის შევსებული!";
-    }
-    ?>
-
-    <a href="index.php">უკან დაბრუნება</a>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['patient_name'];
+            $phone = $_POST['phone'];
+            $doctor = $_POST['doctor_name'];
+            $symptoms = $_POST['symptoms'];
+            $date = $_POST['appointment_date'];
+    
+            echo "<h1>თქვენი ჯავშანი მიღებულია!</h1>";
+            echo "<p><b>პაციენტი:</b> " . $name . "</p>";
+            echo "<p><b>ტელეფონი:</b> " . $phone . "</p>";
+            echo "<p><b>ექიმი:</b> " . $doctor . "</p>";
+            echo "<p><b>სიმპტომები:</b> " . $symptoms . "</p>";
+            echo "<p><b>თარიღი:</b> " . $date . "</p>";
+            
+            echo "<br><hr>";
+            echo "<h3>Confirmation: Your appointment is submitted.</h3>";
+        } else {
+            echo "ფორმა არ არის შევსებული!";
+        }
+        echo '<a href="index.php">უკან დაბრუნება</a>';
+        ?>
+    
+    </div>
 </body>
 </html>
